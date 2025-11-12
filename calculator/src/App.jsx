d8b3878b851d9e7cf4f4cdfb9e8ef8ce9af47c6e
jayti <jaytishah20@gmail.com>
2025-11-12 14:23:13 +0530

    logic + structure

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [value, setValue] = useState("");
+
+  const reset = () => setValue("");
+  const back = () => setValue(value.slice(0, -1));
+
+  const evaluateExpression = () => {
+    try {
+      const result = eval(value);
+      setValue(result.toString());
+    } catch (error) {
+      setValue("Error");
+      console.error(error);
+    }
+  };
+
+  
+  const handleClick = (e) => setValue(value + e.target.value);
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <h2>React Calculator</h2>
+      <input
+        type="text"
+        name="display"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
 
-export default App
+      <div>
+        <button value="1" onClick={handleClick}>1</button>
+        <button value="2" onClick={handleClick}>2</button>
+        <button value="3" onClick={handleClick}>3</button>
+        <button value="/" onClick={handleClick}>/</button>
+
+        <button value="4" onClick={handleClick}>4</button>
+        <button value="5" onClick={handleClick}>5</button>
+        <button value="6" onClick={handleClick}>6</button>
+        <button value="*" onClick={handleClick}>*</button>
+
+        <button value="7" onClick={handleClick}>7</button>
+        <button value="8" onClick={handleClick}>8</button>
+        <button value="9" onClick={handleClick}>9</button>
+        <button value="+" onClick={handleClick}>+</button>
+
+        <button value="0" onClick={handleClick}>0</button>
+        <button value="." onClick={handleClick}>.</button>
+        <button value="-" onClick={handleClick}>-</button>
+
+        <button onClick={evaluateExpression}>=</button>
+        <button onClick={reset}>C</button>
+        <button onClick={back}>⌫</button>
+      </div>
+    </>
+  );
+}
